187429a2a62b7e4c333e30454ed8542020316e0a
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-19 16:41:51 -0300

    Se reutiliza la CARD para renderizar 3 veces. Falta renderizado condicional

--- my-app/src/app/demo/Demo.tsx
+++ my-app/src/app/demo/Demo.tsx
@@ -6,10 +6,17 @@ import { Card } from '../../components/card'    //como se llama index, no se ind
 
 // Creacion del COMPONENTE
 const Demo: React.FC = () => {
+    // se retorna el componente CARD
+    // se RETUTILIZA el componente CARD
+    // se vuelve a RETUTILIZAR el componente CARD per sin descripcion ---> aparece en consola la etique vacía p
+    //? SE debe agregar un renderizado CONDICIONAL en CARD index.tsx 
     return(
         <div>
             <h1>Hola Mundo!</h1>
             <Card title='Card 1' description='This is card 1'/>
+            <Card title='Card 2' description='This is card 2'/>
+            <Card title='Card 3' />
+
         </div>
     );
 }
