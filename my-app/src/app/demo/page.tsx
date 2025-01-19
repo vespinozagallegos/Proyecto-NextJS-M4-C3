ce42ce855368fe60401d3d9876e05330c5f60301
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-19 13:56:54 -0300

    PAGE,RUTA. INDEX,COMPONENTE REUTILIZABLE. Se agrega page como ejemplo de ruta al crear una archivo page dentro de app y demo. Se agrega index para iniciar el uso de componente reutilizable

---
+++ my-app/src/app/demo/page.tsx
@@ -0,0 +1,8 @@
+// Creacion del COMPONENTE
+const Demo: React.FC = () => {
+    return(
+        <h1>Hola Mundo!</h1>
+    );
+}
+
+export default Demo;
---
+++ my-app/src/components/card/index.tsx
@@ -0,0 +1,13 @@
+// vendors
+import React from "react"
+
+// Componente FUNCIONAL
+export const Card: React.FC = () => {
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default Card;
