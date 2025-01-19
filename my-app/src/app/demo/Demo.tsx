88e67016d7840bf4a2eacf092d70116f74fcb2cf
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-19 16:36:08 -0300

    Se crea el componente CARD y se logra renderizar con exito, sin estilos

--- my-app/src/app/demo/Demo.tsx
+++ my-app/src/app/demo/Demo.tsx
@@ -1,7 +1,16 @@
+// vendors
+import React from 'react';
+
+// components
+import { Card } from '../../components/card'    //como se llama index, no se indica el archivo
+
 // Creacion del COMPONENTE
 const Demo: React.FC = () => {
     return(
-        <h1>Hola Mundo!</h1>
+        <div>
+            <h1>Hola Mundo!</h1>
+            <Card title='Card 1' description='This is card 1'/>
+        </div>
     );
 }
 
