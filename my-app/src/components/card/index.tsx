3707debe130137a1c1814eeb2c1c2aa7db62272d
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-19 14:28:24 -0300

    Se completa el FC recibiendo a la interfaz

--- my-app/src/components/card/index.tsx
+++ my-app/src/components/card/index.tsx
@@ -1,11 +1,17 @@
 // vendors
 import React from "react"
 
-// Componente FUNCIONAL
-export const Card: React.FC = () => {
+// types
+import { ICardProps } from "./types";
+
+// Componente FUNCIONAL con el valor que recibe <> --->  asignado desde la interfaz
+// se desestructuran los valores recibidos como params ()
+export const Card: React.FC<ICardProps> = ({ title, description }) => {
+    // se renderizan los params recibidos
     return(
         <div>
-            
+            <h2>{title}</h2>
+            <p>{description}</p>
         </div>
     );
 }
