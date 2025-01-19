ad48b8ab030d5434dd2c54d9a8e26711059746bd
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-19 16:45:20 -0300

    Se agrega renderizado condicional para mejorar RENDIMIENTO DEL DOM

--- my-app/src/components/card/index.tsx
+++ my-app/src/components/card/index.tsx
@@ -8,10 +8,11 @@ import { ICardProps } from "./types";
 // se desestructuran los valores recibidos como params ()
 export const Card: React.FC<ICardProps> = ({ title, description }) => {
     // se renderizan los params recibidos
+    //? se agrega renderizado condicional para mejorar RENDIMIENTO DEL DOM
     return(
         <div>
             <h2>{title}</h2>
-            <p>{description}</p>
+            {description && <p>{description}</p>}
         </div>
     );
 }
